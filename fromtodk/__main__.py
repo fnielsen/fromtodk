58fe5f179a8a42f938306ef475686099694e96f5
fnielsen <fn@imm.dtu.dk>
2016-08-15 20:46:25 +0200

    Create __main__.py

---
+++ fromtodk/__main__.py
@@ -0,0 +1,49 @@
+"""fromtodk
+
+Usage:
+  fromtodk [options] <from> <to>
+
+Options:
+  -h --help     Print help message
+  -v --verbose  Verbose
+
+Examples:
+  $ python -m fromtodk "Lyngby Hovedgade" Aros
+  150.135086498
+
+"""
+
+from __future__ import division, print_function
+
+import sys
+reload(sys)
+sys.setdefaultencoding("utf-8")
+
+from docopt import docopt
+
+from .wikidata import search_wikidata_entities, get_distance_from_qids
+
+
+def main():
+    """Handel command-line interface."""
+
+    args = docopt(__doc__)
+
+    verbose = args['--verbose']
+    
+    from_qids = search_wikidata_entities(args['<from>'])
+    if verbose:
+        print(from_qids)
+    to_qids = search_wikidata_entities(args['<to>'])
+    if verbose:
+        print(to_qids)
+    
+    distance = get_distance_from_qids(from_qids[0], to_qids[0])
+    if distance:
+        print(distance)
+    elif distance is None:
+        print()
+
+
+if __name__ == '__main__':
+    main()
